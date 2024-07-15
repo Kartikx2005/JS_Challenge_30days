c0327a4f9cac8e87575b2adb4e2a739029d25edd
Kartikx2005 <kartikxdev2005@gmail.com>
2024-07-15 10:25:33 +0530

    completed day - 2

---
+++ day_02.js
@@ -0,0 +1,75 @@
+"use strict";
+// Operators
+
+// Activity 1 : Arithmetic Operations
+
+// Task - 1
+let num1 = 10
+let num2 = 20
+console.log(num1 + num2)
+
+// Task - 2
+console.log(num1 - num2)
+
+// Task - 3
+console.log(num1 * num2)
+
+// Task - 4
+console.log(num1 / num2)
+
+// Task - 5
+console.log(num1 % num2)
+
+// Activity 2 : Assignment Operators 
+
+// Task - 6
+let a = 4
+let b = 5
+a += b
+console.log(a)
+
+// Task - 7
+let c = 10
+let d = 7
+c -= d
+console.log(c)
+
+// Activity 3 : Comaprison Operators
+
+// Task - 8
+a = 10
+b = 20
+console.log(`Is a greater than b : ${a>b}`)
+console.log(`Is a smaller then b : ${a<b}`)
+
+// Task - 9
+let x = 45
+let y = 15
+console.log(`Is x greater than or equal to y : ${x>=y}`)
+console.log(`Is x smaller than or equal to y : ${x<=y}`)
+
+// Task - 10
+x = "10"
+y = 10
+console.log(`is value of x and y is same : ${x==y}`)
+console.log(`is value and type of x and y is same : ${x===y}`)
+
+// Activity 4 : Logical Operators 
+
+// Task - 11
+a = 10
+b = 20
+x = 50
+y = 30
+
+console.log(`is a is grater then b and x is greater then y : ${(a>b)&&(x>y)}`)
+
+// Task - 12
+console.log(`is a is grater then b or x is greater then y : ${(a>b)||(x>y)}`)
+
+// Task - 13
+console.log(`Negative of true is : ${!(true)}`)
+
+// Task - 14
+let r = 10>=0 ? "No is positive" : "No is negative"
+console.log(r)
